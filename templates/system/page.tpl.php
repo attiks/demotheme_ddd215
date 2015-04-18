2228bd03a574f7959e804a643fab3a17e5c75ae9
Peter Droogmans <peter@attiks.com>
2015-04-18 10:29:55 +0200

    feed icon removed

--- templates/system/page.tpl.php
+++ templates/system/page.tpl.php
@@ -112,7 +112,6 @@
         <ul class="action-links"><?php print render($action_links); ?></ul>
       <?php endif; ?>
       <?php print render($page['content']); ?>
-      <?php print $feed_icons; ?>
     </div>
 
     <?php print render($page['sidebar_first']); ?>
