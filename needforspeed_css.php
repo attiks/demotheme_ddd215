b08c2fe87bc6f38214d2321da02a4fb5c314ea1a
Peter Droogmans <peter@attiks.com>
2015-05-13 15:11:04 +0200

    use path_to_theme in include files

--- needforspeed_css.php
+++ needforspeed_css.php
@@ -1,13 +1,13 @@
 <?php if (isset($GLOBALS['needforspeed']) && $GLOBALS['needforspeed']): ?> 
   <style>
-    <?php include 'sites/all/themes/custom/demotheme/css/critical-min.css'; ?>
+    <?php include path_to_theme() . '/css/critical-min.css'; ?>
   </style>
   <script>
     if(window.document.cookie&&window.document.cookie.indexOf('fonts-loaded')>-1&&window.document.documentElement.className.indexOf('fonts-loaded')===-1){window.document.documentElement.className+=' fonts-loaded';};
-    <?php include 'sites/all/themes/custom/demotheme/jslib/cssloader.js'; ?>
-    loadCSS("/sites/all/themes/custom/demotheme/css/site_1429341975052.css");
-    <?php include 'sites/all/themes/custom/demotheme/jslib/jsloader.js'; ?>
-  </script><noscript><link href="/sites/all/themes/custom/demotheme/css/site_1429341975052.css" rel="stylesheet"></noscript>
+    <?php include path_to_theme() . '/jslib/cssloader.js'; ?>
+    loadCSS("/<?php print path_to_theme() ?>/css/site_1431522443143.css");
+    <?php include path_to_theme() . '/jslib/jsloader.js'; ?>
+  </script><noscript><link href="/<?php print path_to_theme() ?>/css/site_1431522443143.css" rel="stylesheet"></noscript>
 <?php else: ?>
   <?php print $styles; ?>
 <?php endif; ?>
--- needforspeed_script.php
+++ needforspeed_script.php
@@ -1,6 +1,6 @@
 <?php if (isset($GLOBALS['needforspeed']) && $GLOBALS['needforspeed']): ?> 
   <?php print $scripts; ?>
-  <script async src="/<?php print path_to_theme() ?>/js/script.min_1429341975052.js"></script>
+  <script async src="/<?php print path_to_theme() ?>/js/script.min_1431522443143.js"></script>
 <?php else: ?>
   <?php print $scripts; ?>
 <?php endif; ?>
