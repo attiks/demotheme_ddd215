4b01d50e0519bc1d67c433ef013009bf99d9956d
Peter Droogmans <peter@attiks.com>
2015-06-08 22:57:55 +0200

    clean up renames

--- rename.sh
+++ rename.sh
@@ -1,12 +1,9 @@
-sed -i 's/demotheme/'"$1"'/g' bower.json
 sed -i 's/demotheme/'"$1"'/g' sass/demotheme.no-query.scss
 sed -i 's/demotheme/'"$1"'/g' theme-settings.php
 sed -i 's/demotheme/'"$1"'/g' process/page.process.inc
-sed -i 's/demotheme/'"$1"'/g' js/demotheme.behaviors.js
 sed -i 's/demotheme/'"$1"'/g' preprocess/page.preprocess.inc
 sed -i 's/demotheme/'"$1"'/g' package.json
 sed -i 's/demotheme/'"$1"'/g' demotheme.info
-sed -i 's/demotheme/'"$1"'/g' needforspeed_css.php
 sed -i 's/demotheme/'"$1"'/g' template.php
 
 rename 's/demotheme/'"$1"'/' demotheme.info
